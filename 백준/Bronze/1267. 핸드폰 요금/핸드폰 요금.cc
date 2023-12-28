7e3288d4c408a21acbb4e7a051dd30b5ea9bb824
cny1213 <80268127+cny1213@users.noreply.github.com>
2023-12-28 22:50:37 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+
+	int N, yPrice=0, mPrice=0;
+	string result;
+	int price = 0;
+	//1. "동호의 저번 달 통화 개수" N 을 입력받는다
+	cin >> N;
+	int* callTime = new int[N];
+	//2. N의 개수 만큼 통화시간을 입력받는다. (for문 이용)
+	for (int i = 0; i < N; i++) {
+		cin >> callTime[i];
+
+		int yPrice_temp = (callTime[i] / 30 + 1) * 10;
+		yPrice += yPrice_temp;
+		int mPrice_temp = (callTime[i] / 60 + 1) * 15;
+		mPrice += mPrice_temp;
+	}
+
+
+	if (yPrice == mPrice) {
+		cout <<  "Y M " << yPrice;
+	}
+	else {
+		price = (yPrice < mPrice) ? yPrice : mPrice;
+		result = (yPrice < mPrice) ? "Y" : "M";
+
+		cout << result << " " << price;
+	}
+
+	return 0;
+}
